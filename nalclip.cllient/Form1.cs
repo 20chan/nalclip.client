bdd0488f4bda0002ebd01931335a91677f9b01dd
20chan <2@0chan.dev>
2021-03-20 04:02:01 +0900

    일단 클립보드 이벤트에서 웹으로 쏘는거까지

--- nalclip.cllient/Form1.cs
+++ nalclip.cllient/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +27,21 @@ namespace nalclip.cllient {
         }
 
         private void OnCopy() {
+            var value = new Clip { content = Clipboard.GetText() };
+            Task.Run(async () => {
+                var client = new HttpClient();
+                var content = new StringContent(
+                    JsonConvert.SerializeObject(value),
+                    Encoding.UTF8,
+                    "application/json"
+                );
+                System.Diagnostics.Debug.WriteLine($"before ${JsonConvert.SerializeObject(value)}");
+                var resp = await client.PostAsync("https://clip.0ch.me/api/clip", content);
+                System.Diagnostics.Debug.WriteLine($"after ${resp.StatusCode}");
+                var a = await resp.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine($"after ${a}");
+
+            });
         }
 
         protected override void WndProc(ref Message m) {
@@ -47,5 +64,9 @@ namespace nalclip.cllient {
                     break;
             }
         }
+    }
+
+    struct Clip {
+        public string content;
     }
 }
